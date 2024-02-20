a569969f5f0c8719130c0a4f50758a34a55da66e
Inun572 <helmi.ainun@gmail.com>
2024-02-20 10:54:12 +0700

    add theme context

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import { RouterProvider } from "react-router-dom"
 import Router from "./router"
+import { ColorModeContext, useMode } from "./theme"
+import { CssBaseline, ThemeProvider } from "@mui/material"
 
 const App = () => {
-  return <RouterProvider router={Router} />
+  const [theme, colorMode] = useMode()
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <RouterProvider router={Router} />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  )
 }
 
 export default App
