a92d60495004098c1960f02e17c254ca998a5d6f
Inun572 <helmi.ainun@gmail.com>
2024-02-20 16:44:18 +0700

    fix theme settings and set inital navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,23 @@
-import { Box, IconButton, InputBase, Typography, useTheme } from "@mui/material"
-import { tokens } from "../theme"
-import { Search } from "@mui/icons-material"
+import { Box, Button, IconButton, InputBase, Typography, useTheme } from "@mui/material"
+import { ColorModeContext, tokens } from "../theme"
+import {
+  DarkModeOutlined,
+  LightModeOutlined,
+  Search,
+  ShoppingCartOutlined
+} from "@mui/icons-material"
+import { useContext } from "react"
 
 const Navbar = () => {
   const theme = useTheme()
-  const colors = tokens(theme.palette.mode)
+  const colors = tokens(theme.pallete.mode)
+  const colorMode = useContext(ColorModeContext)
   return (
     <header>
       <nav>
-        <Box
-          display="flex"
-          justifyContent="space-between"
-          alignItems="center"
-          p={2}
-          bgcolor={colors.teal[500]}
-        >
+        <Box display="flex" justifyContent="space-between" alignItems="center" p={3}>
           <Box>
-            <Typography variant="h5" color={colors.grey[500]}>
-              E-Commerce Logo
-            </Typography>
+            <Typography variant="h5">E-Commerce Logo</Typography>
           </Box>
           <Box display="flex" gap="20px" alignItems="center">
             <InputBase
@@ -27,15 +26,28 @@ const Navbar = () => {
                 paddingY: "2px",
                 paddingLeft: "20px",
                 fontSize: "14px",
-                color: colors.grey[400],
-                bgcolor: colors.white[800],
                 borderRadius: "8px"
               }}
               placeholder="Search"
             />
             <IconButton>
-              <Search sx={{ color: colors.grey[500] }} fontSize="small" />
+              <Search fontSize="small" />
             </IconButton>
+          </Box>
+          <Box>
+            <IconButton>
+              <ShoppingCartOutlined sx={{ color: colors.primary[900] }} />
+            </IconButton>
+            <IconButton onClick={colorMode.toggleColorMode}>
+              {theme.pallete.mode === "dark" ? (
+                <LightModeOutlined sx={{ color: "orange" }} />
+              ) : (
+                <DarkModeOutlined sx={{ color: "indigo" }} />
+              )}
+            </IconButton>
+            <Button variant="contained" sx={{ bgcolor: colors.secondary[400] }}>
+              Login
+            </Button>
           </Box>
         </Box>
       </nav>
