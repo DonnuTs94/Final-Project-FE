4e186fd57d10b8142ad3ae646df29f2a48560a96
Inun572 <helmi.ainun@gmail.com>
2024-02-21 17:31:59 +0700

    add order admin dashboard

--- src/pages/AdminDashboard/Order.jsx
+++ src/pages/AdminDashboard/Order.jsx
@@ -1,5 +1,7 @@
 import {
   Box,
+  Button,
+  Modal,
   Paper,
   Table,
   TableBody,
@@ -12,15 +14,19 @@ import {
 } from "@mui/material"
 import { useState, useEffect } from "react"
 import { axiosInstance } from "../../configs/api/api"
+import { VisibilityOffOutlined } from "@mui/icons-material"
 
 const tokenAdmin =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTksInJvbGVJZCI6MTMsImlhdCI6MTcwODMzNDMwNSwiZXhwIjoxNzEwOTI2MzA1fQ.G505TR_v9a7NoKRy9rYVpBA8T4KEUylFci3PmrngZC4"
 
 const OrderPage = () => {
   const [orders, setOrders] = useState([])
-  const [loading, setLoading] = useState(false)
   const [page, setPage] = useState(0)
   const [rowsPerPage, setRowsPerPage] = useState(10)
+  const [open, setOpen] = useState(false)
+  const [orderDetail, setOrderDetail] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [loadingDetail, setLoadingDetail] = useState(false)
 
   useEffect(() => {
     getAllOrders()
@@ -43,7 +49,23 @@ const OrderPage = () => {
     setLoading(false)
   }
 
-  const handleChangePage = (event, newPage) => {
+  const getOrderDetailById = async (orderId) => {
+    setLoadingDetail(true)
+    try {
+      const response = await axiosInstance.get(`orders/admin/${Number(orderId)}`, {
+        headers: {
+          Authorization: `Bearer ${tokenAdmin}`,
+          "Content-Type": "application/json"
+        }
+      })
+      setOrderDetail(response.data.order)
+      setLoadingDetail(false)
+    } catch (err) {
+      setLoadingDetail(false)
+    }
+  }
+
+  const handleChangePage = (newPage) => {
     setPage(newPage)
   }
 
@@ -52,16 +74,22 @@ const OrderPage = () => {
     setPage(0)
   }
 
+  const openModal = async (orderId) => {
+    setOpen(true)
+    await getOrderDetailById(orderId)
+  }
+
   const columns = [
-    { id: "orderId", label: "Order ID", minWidth: 120, align: "left" },
-    { id: "invoice", label: "No. Invoice", minWidth: 120, align: "left" },
+    { id: "orderId", label: "Order ID", minWidth: 60, align: "left" },
+    { id: "invoice", label: "No. Invoice", minWidth: 60, align: "left" },
     { id: "date", label: "Order Date", minWidth: 120, align: "left" },
     { id: "userId", label: "User ID", minWidth: 120, align: "left" },
     { id: "destination", label: "Destination ID", minWidth: 120, align: "left" },
     { id: "totalOrder", label: "Total Order", minWidth: 120, align: "left" },
     { id: "totalOngkir", label: "Total Ongkir", minWidth: 120, align: "left" },
     { id: "grandTotal", label: "Grand Total", minWidth: 120, align: "left" },
-    { id: "status", label: "Order Status", minWidth: 120, align: "left" }
+    { id: "status", label: "Order Status", minWidth: 120, align: "left" },
+    { id: "action", label: "Action", minWidth: 120, align: "left" }
   ]
   return (
     <Box width={"100%"}>
@@ -84,32 +112,42 @@ const OrderPage = () => {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {orders.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                {orders.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((order) => {
                   return (
-                    <TableRow hover key={row.id}>
-                      <TableCell>{row.id}</TableCell>
-                      <TableCell>{row.invoice}</TableCell>
+                    <TableRow hover key={order.id}>
+                      <TableCell>{order.id}</TableCell>
+                      <TableCell>{order.invoice}</TableCell>
                       <TableCell>
-                        {Intl.DateTimeFormat("id-ID").format(new Date(row.date))}
+                        {Intl.DateTimeFormat("id-ID").format(new Date(order.date))}
                       </TableCell>
-                      <TableCell>{row.userId}</TableCell>
-                      <TableCell>{row.destination}</TableCell>
+                      <TableCell>{order.userId}</TableCell>
+                      <TableCell>{order.destination}</TableCell>
                       <TableCell>
                         {Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR" }).format(
-                          row.totalOrder * 16000
+                          order.totalOrder * 16000
                         )}
                       </TableCell>
                       <TableCell>
                         {Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR" }).format(
-                          row.totalOngkir
+                          order.totalOngkir
                         )}
                       </TableCell>
                       <TableCell>
                         {Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR" }).format(
-                          row.grandTotal
+                          order.grandTotal
                         )}
                       </TableCell>
-                      <TableCell>{row.status}</TableCell>
+                      <TableCell>{order.status}</TableCell>
+                      <TableCell>
+                        <Button
+                          variant="contained"
+                          color="success"
+                          onClick={() => openModal(order.id)}
+                        >
+                          <VisibilityOffOutlined />
+                          Detail
+                        </Button>
+                      </TableCell>
                     </TableRow>
                   )
                 })}
@@ -128,6 +166,97 @@ const OrderPage = () => {
           />
         </Paper>
       )}
+
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            padding: 2,
+            minWidth: 600,
+            minHeight: 300,
+            bgcolor: "background.paper",
+            display: "flex",
+            flexDirection: "column",
+            borderRadius: "8px"
+          }}
+        >
+          <Typography variant="h4" color="primary">
+            Order Details
+          </Typography>
+          {loadingDetail ? (
+            <Typography>Loading...</Typography>
+          ) : (
+            <>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  gap: 2
+                }}
+              >
+                <Box color="primary">
+                  <Typography variant="h5">Order Information</Typography>
+                  <Typography>Order ID: {orderDetail.id} </Typography>
+                  <Typography>Invoice Number: {orderDetail.id} </Typography>
+                  <Typography>Order Date: {orderDetail.date} </Typography>
+                  <Typography>Destination ID: {orderDetail.destination} </Typography>
+                  <Typography>Order Status: {orderDetail.status} </Typography>
+                </Box>
+                <Box>
+                  <Typography variant="h5">User Details</Typography>
+                  <Typography>{orderDetail.userId}</Typography>
+                </Box>
+              </Box>
+              <Box>
+                <TableContainer>
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>No.</TableCell>
+                        <TableCell>Product ID</TableCell>
+                        <TableCell>Quantity</TableCell>
+                        <TableCell>Price</TableCell>
+                        <TableCell>Total</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {orderDetail.orderItem.map((item, index) => {
+                        return (
+                          <TableRow key={item.id}>
+                            <TableCell>{index + 1}</TableCell>
+                            <TableCell>{item.productId}</TableCell>
+                            <TableCell>{item.quantity}</TableCell>
+                            <TableCell>{item.price}</TableCell>
+                            <TableCell>{item.total}</TableCell>
+                          </TableRow>
+                        )
+                      })}
+                      <TableRow>
+                        <TableCell colSpan={2}></TableCell>
+                        <TableCell colSpan={2}>Subtotal</TableCell>
+                        <TableCell>{orderDetail.totalOrder}</TableCell>
+                      </TableRow>
+                      <TableRow>
+                        <TableCell colSpan={2}></TableCell>
+                        <TableCell colSpan={2}>Ongkir</TableCell>
+                        <TableCell>{orderDetail.totalOngkir}</TableCell>
+                      </TableRow>
+                      <TableRow>
+                        <TableCell colSpan={2}></TableCell>
+                        <TableCell colSpan={2}>Grand Total</TableCell>
+                        <TableCell>{orderDetail.grandTotal}</TableCell>
+                      </TableRow>
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </Box>
+            </>
+          )}
+        </Box>
+      </Modal>
     </Box>
   )
 }
