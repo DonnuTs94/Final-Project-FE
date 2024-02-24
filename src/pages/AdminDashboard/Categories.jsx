b8e01e014c0cff4721312b63fce64c6b0dbc4ac9
AndrianaRzky16 <andrianarizki42@gmail.com>
2024-02-24 16:05:30 +0700

    User Admin

--- src/pages/AdminDashboard/Categories.jsx
+++ src/pages/AdminDashboard/Categories.jsx
@@ -4,9 +4,7 @@ import StickyHeadTable from "../../components/CategoryTable"
 const CategoryPage = () => {
   return (
     <Box width={"100%"}>
-      <Typography marginBottom={5} variant="h3">
-        Category Page
-      </Typography>
+      <Typography marginBottom={5} variant="h3"></Typography>
       <StickyHeadTable />
     </Box>
   )
