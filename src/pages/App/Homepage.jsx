326a1660c03ef016c0eb0e0c5310a401c857078e
Evan Morningstar <evankhell619@gmail.com>
2024-02-27 00:52:02 +0700

    add homepage productlist

--- src/pages/App/Homepage.jsx
+++ src/pages/App/Homepage.jsx
@@ -1,9 +1,11 @@
 import Banners from "../../components/Banners"
+import ProductList from "../Product/ProductList"
 
 const Homepage = () => {
   return (
     <>
       <Banners />
+      <ProductList />
     </>
   )
 }
