724e2f04b1cf986952878299cc1fd14d3063c41a
Inun572 <helmi.ainun@gmail.com>
2024-02-28 13:43:23 +0700

    fix infinite fetching, and redirect after logout

--- src/pages/ProtectThisRoute.jsx
+++ src/pages/ProtectThisRoute.jsx
@@ -1,16 +1,3 @@
-// import { useSelector } from "react-redux"
-// import { Navigate, Outlet } from "react-router-dom"
-
-// const ProtectThisRoute = () => {
-//   const { userData } = useSelector((state) => state.users)
-
-//   console.log(userData)
-
-//   return userData?.Role?.name === "admin" ? <Outlet /> : <Navigate to="/" />
-// }
-
-// export default ProtectThisRoute
-
 import { useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { Navigate, Outlet } from "react-router-dom"
@@ -30,14 +17,7 @@ const ProtectThisRoute = () => {
     if (localStorage.getItem("auth_token")) {
       dispatch(getUserData())
     }
-  }, [dispatch, userData])
-
-  useEffect(() => {
-    // Log userData, loading, and error for debugging purposes
-    console.log("userData:", userData)
-    console.log("loading:", loading)
-    console.log("error:", error)
-  }, [userData, loading, error])
+  }, [])
 
   // Handle loading state
   if (loading) {
@@ -51,7 +31,7 @@ const ProtectThisRoute = () => {
 
   // Redirect if userData is null or user is not an admin
   if (!userData || (userData.Role && userData.Role.name !== "admin")) {
-    return <Navigate to="/*" />
+    return <Navigate to="/" />
   }
 
   // Render Outlet if user is authenticated as an admin
